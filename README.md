3cf2a3d949e3b91c0f54e77dec899d12f2557f5d
Franciscolacarnum <franklinz622@gmail.com>
2019-04-14 01:05:16 -0400

    first release

--- README.md
+++
@@ -1,3 +0,0 @@
-# CAIRN
-Cairn project by Weiyi Li rebuilt for UCCA exhibition
-https://franklinzhu.github.io/phenomenalab/
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'BootStrap'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
